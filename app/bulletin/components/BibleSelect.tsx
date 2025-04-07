b69a92e284aac22346acd6e7ab73255eb914bcee
Awesomeyelim <uiop01900@gmail.com>
2025-04-07 21:42:21 +0900

    multi selected verse

--- app/bulletin/components/BibleSelect.tsx
+++ app/bulletin/components/BibleSelect.tsx
@@ -11,32 +11,33 @@ type Selection = {
 };
 
 interface BibleSelectProps {
-  handleValueChange: (key: string, { newObj, newLead }: { newObj: string; newLead?: string }) => void;
+  handleValueChange: (
+    key: string,
+    { newObj, newLead }: { newObj: string; newLead?: string }
+  ) => void;
   parentKey: string;
 }
 
 type BibleKey = keyof typeof bibleData;
-const BibleSelect: React.FC<BibleSelectProps> = ({ handleValueChange, parentKey }) => {
+const BibleSelect: React.FC<BibleSelectProps> = ({
+  handleValueChange,
+  parentKey,
+}) => {
   const selectedDetail = useRecoilValue(selectedDetailState);
   const [book, chapterverse] = selectedDetail.obj.split("_");
   let [chapter, verse] = chapterverse.split(":");
   const isRanged = verse.includes("-");
 
-  const [selectedBook, setSelectedBook] = useState<Selection>({
-    book,
-    chapter: +chapter,
-    verse: isRanged ? +verse.split("-")[0] : +verse,
-  });
+  const [selectedBook, setSelectedBook] = useState<Selection>({ book });
   const selectedInit = isRanged
     ? [
-        selectedBook,
-        {
-          ...selectedBook,
-          verse: +verse.split("-")[1],
-        },
+        { book, chapter: +chapter, verse: +verse.split("-")[0] },
+        { book, chapter: +chapter, verse: +verse.split("-")[1] },
       ]
-    : [selectedBook];
-  const [selectedRanges, setSelectedRanges] = useState<Selection[]>(selectedInit);
+    : [{ book, chapter: +chapter, verse: +verse }];
+  const [selectedRanges, setSelectedRanges] =
+    useState<Selection[]>(selectedInit);
+  const [multiSelection, setMultiSelection] = useState<Selection[][]>([]);
 
   const handler = {
     bookChange: (event: React.ChangeEvent<HTMLSelectElement>) => {
@@ -54,35 +55,53 @@ const BibleSelect: React.FC<BibleSelectProps> = ({ handleValueChange, parentKey 
       setSelectedBook({ ...selectedBook, verse: Number(event.target.value) });
     },
     addSelection: () => {
-      if (selectedBook.book && selectedBook.chapter > 0 && selectedBook.verse > 0) {
-        setSelectedRanges((prevRanges) => {
-          const updatedRanges = [...prevRanges, selectedBook];
-          const first = updatedRanges[0];
-          const last = updatedRanges[1];
-
-          if (updatedRanges.length > 1) {
-            handleValueChange(parentKey, {
-              newObj: `${bibleData[selectedBook.book as BibleKey].kor}_${
-                bibleData[selectedBook.book as BibleKey].index
-              }/${first.chapter}:${first.verse}-${last.chapter}:${last.verse}`,
-            });
-          } else {
-            handleValueChange(parentKey, {
-              newObj: `${bibleData[selectedBook.book as BibleKey].kor}_${
-                bibleData[selectedBook.book as BibleKey].index
-              }/${first.chapter}:${first.verse}`,
-            });
-          }
-
-          return updatedRanges;
-        });
-        setSelectedBook({ book: selectedBook.book, chapter: 0, verse: 0 }); // 초기화
+      if (
+        selectedBook.book &&
+        selectedBook.chapter > 0 &&
+        selectedBook.verse > 0
+      ) {
+        setSelectedRanges((prev) => [...prev, selectedBook]);
+        setSelectedBook({ book: selectedBook.book, chapter: 0, verse: 0 });
       }
+    },
+    finalizeSelection: () => {
+      if (selectedRanges.length === 0) return;
+
+      setMultiSelection((prev) => {
+        const updated = [...prev, selectedRanges];
+
+        const finalObj = updated
+          .map((ranges) => {
+            const first = ranges[0];
+            const last = ranges[1] || first;
+            const kor = bibleData[first.book as BibleKey].kor;
+            const index = bibleData[first.book as BibleKey].index;
+            return (
+              `${kor}_${index}/${first.chapter}:${first.verse}` +
+              (ranges.length > 1 ? `-${last.chapter}:${last.verse}` : "")
+            );
+          })
+          .join(", ");
+
+        handleValueChange(parentKey, {
+          newObj: finalObj,
+        });
+
+        return updated;
+      });
+
+      setSelectedRanges([]);
+      setSelectedBook({ book: "", chapter: 0, verse: 0 });
     },
   };
 
-  const currentBook = selectedBook.book ? bibleData[selectedBook.book as BibleKey] : null;
-  const currentChapterVerses = currentBook && selectedBook.chapter ? currentBook.chapters[selectedBook.chapter - 1] : 0;
+  const currentBook = selectedBook.book
+    ? bibleData[selectedBook.book as BibleKey]
+    : null;
+  const currentChapterVerses =
+    currentBook && selectedBook.chapter
+      ? currentBook.chapters[selectedBook.chapter - 1]
+      : 0;
 
   const formatRange = (ranges: Selection[]) => {
     return ranges
@@ -98,95 +117,135 @@ const BibleSelect: React.FC<BibleSelectProps> = ({ handleValueChange, parentKey 
   };
 
   return (
-    <div className="bible-select-container">
-      <h3 className="title">성경 구절 선택</h3>
-      {selectedRanges.length !== 2 && (
-        <>
-          <div className="select-group">
-            <label className="select-label">
-              책 선택:
-              <select
-                className="select-box"
-                onChange={handler.bookChange}
-                value={selectedRanges.length > 0 ? selectedRanges[0].book : selectedBook.book || ""}>
-                <option value="" disabled>
-                  책을 선택하세요
-                </option>
-                {selectedRanges.length > 0 ? (
-                  <option value={bibleData[selectedRanges[0]?.book as BibleKey].kor}>
-                    {bibleData[selectedRanges[0]?.book as BibleKey].kor}
+    <>
+      <div className="bible-select-container">
+        <h3 className="title">성경 구절 선택</h3>
+        {selectedRanges.length !== 2 && (
+          <>
+            <div className="select-group">
+              <label className="select-label">
+                책 선택:
+                <select
+                  className="select-box"
+                  onChange={handler.bookChange}
+                  value={selectedBook.book || ""}
+                >
+                  <option value="" disabled>
+                    책을 선택하세요
                   </option>
-                ) : (
-                  Object.entries(bibleData).map(([key, value]) => (
+                  {Object.entries(bibleData).map(([key, value]) => (
                     <option key={key} value={key}>
                       {value.kor}
                     </option>
-                  ))
-                )}
-              </select>
-            </label>
-
-            {currentBook && (
-              <label className="select-label">
-                장 선택:
-                <select className="select-box" onChange={handler.chapterChange} value={selectedBook.chapter || ""}>
-                  <option value="" disabled>
-                    장을 선택하세요
-                  </option>
-                  {currentBook.chapters.map((_, index) => (
-                    <option key={index} value={index + 1} disabled={selectedRanges[0]?.chapter > index + 1}>
-                      {index + 1}장
-                    </option>
                   ))}
                 </select>
               </label>
-            )}
 
-            {currentBook && selectedBook.chapter > 0 && (
-              <label className="select-label">
-                절 선택:
-                <select className="select-box" onChange={handler.verseChange} value={selectedBook.verse || ""}>
-                  <option value="" disabled>
-                    절을 선택하세요
-                  </option>
-                  {Array.from({ length: currentChapterVerses }, (_, i) => i + 1).map((verse, index) => (
-                    <option
-                      key={verse}
-                      value={verse}
-                      disabled={
-                        selectedRanges[0]?.chapter == selectedBook.chapter ? selectedRanges[0]?.verse > index : false
-                      }>
-                      {verse}절
+              {currentBook && (
+                <label className="select-label">
+                  장 선택:
+                  <select
+                    className="select-box"
+                    onChange={handler.chapterChange}
+                    value={selectedBook.chapter || ""}
+                  >
+                    <option value="" disabled>
+                      장을 선택하세요
                     </option>
-                  ))}
-                </select>
-              </label>
-            )}
-          </div>
+                    {currentBook.chapters.map((_, index) => (
+                      <option key={index} value={index + 1}>
+                        {index + 1}장
+                      </option>
+                    ))}
+                  </select>
+                </label>
+              )}
+
+              {currentBook && selectedBook.chapter > 0 && (
+                <label className="select-label">
+                  절 선택:
+                  <select
+                    className="select-box"
+                    onChange={handler.verseChange}
+                    value={selectedBook.verse || ""}
+                  >
+                    <option value="" disabled>
+                      절을 선택하세요
+                    </option>
+                    {Array.from(
+                      { length: currentChapterVerses },
+                      (_, i) => i + 1
+                    ).map((verse) => (
+                      <option key={verse} value={verse}>
+                        {verse}절
+                      </option>
+                    ))}
+                  </select>
+                </label>
+              )}
+            </div>
+            <button
+              className="add-selection-button"
+              onClick={handler.addSelection}
+              disabled={
+                !(
+                  selectedBook.book &&
+                  selectedBook.chapter > 0 &&
+                  selectedBook.verse > 0
+                )
+              }
+            >
+              추가
+            </button>
+          </>
+        )}
+        {selectedRanges.length > 0 && (
           <button
             className="add-selection-button"
-            onClick={handler.addSelection}
-            disabled={!(selectedBook.book && selectedBook.chapter > 0 && selectedBook.verse > 0)}>
-            추가
+            onClick={() => {
+              setSelectedRanges([]);
+              setSelectedBook({ book: "", chapter: 0, verse: 0 });
+            }}
+          >
+            다시 선택
           </button>
-        </>
-      )}
-      {selectedRanges.length > 0 && (
-        <button
-          className="add-selection-button"
-          onClick={() => {
-            setSelectedRanges([]);
-            setSelectedBook({ book: "", chapter: 0, verse: 0 });
-          }}>
-          다시 선택
-        </button>
-      )}
-      {selectedRanges.length > 0 && (
-        <div className="result-container">
-          <p className="result-text">{formatRange(selectedRanges)}</p>
+        )}
+        {selectedRanges.length > 0 && (
+          <div className="result-container">
+            <p className="result-text">{formatRange(selectedRanges)}</p>
+          </div>
+        )}
+      </div>
+      <button
+        className="add-selection-button"
+        onClick={handler.finalizeSelection}
+      >
+        구절 추가
+      </button>
+      {multiSelection.length > 0 && (
+        <div className="multi-selection-list">
+          {multiSelection.map((ranges, index) => {
+            const first = ranges[0];
+            const last = ranges[1] || first;
+            const kor = bibleData[first.book as BibleKey].kor;
+
+            const displayText =
+              `${kor} ${first.chapter}:${first.verse}` +
+              (ranges.length > 1
+                ? `-${
+                    first.chapter === last.chapter ? "" : `${last.chapter}:`
+                  }${last.verse}`
+                : "");
+
+            return (
+              <span key={index} className="verse-chip">
+                📖 {displayText}
+              </span>
+            );
+          })}
         </div>
       )}
-    </div>
+    </>
   );
 };
 
